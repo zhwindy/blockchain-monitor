21812513085613428e25e19d0f1fe175a6c8a20f
tn_pengfei.zhao <zhaopengfei@tn-pengfeizhaodeMacBook-Pro.local>
2022-02-26 17:59:56 +0800

    fix time calc

--- crontab/monitor_process_height.py
+++ crontab/monitor_process_height.py
@@ -4,6 +4,7 @@ import datetime
 import time
 import telegram
 import os
+import pytz
 
 host = "nftscan-source-db-instance-1.cjotu4lyzmfi.ap-southeast-1.rds.amazonaws.com"
 
@@ -41,11 +42,12 @@ def main():
     now_timestamp = int(time.time())
     block_number = info.get("block_number")
     record_time = info.get("create_time")
-    record_timestamp = int(datetime.datetime.timestamp(record_time))
+    # record_timestamp = int(datetime.datetime.timestamp(record_time))
+    record_timestamp = int(record_time.replace(tzinfo=pytz.timezone('Asia/Shanghai')).timestamp())
     diff_seconds = max(0, now_timestamp-record_timestamp)
     diff_min = diff_seconds // 60
     text =f"【解析延迟告警】主链:ETH\n已解析高度: {block_number}\n当前已延迟: {diff_min}分钟\n请核实相关情况, 及时处理!"
-    print(text)
+    # print(text)
     bot = telegram.Bot(token=token)
     bot.send_message(text=text, chat_id=group_id)
 
