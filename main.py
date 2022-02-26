c89a0e43d7243e567e161f75d1f143bc10b98a96
tn_pengfei.zhao <zhaopengfei@tn-pengfeizhaodeMacBook-Pro.local>
2022-02-26 16:31:33 +0800

    add task

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def bsc1(update: Update, context: CallbackContext):
     block_height = get_block_height(node_data)
     block_time = get_block_time(node_data)
     text = f"节点: BSC-01\n最新高度: {block_height}\n出块时间: {block_time}"""
-    logging.info(effective_chat)
+    logging.info(update.effective_chat)
     chat_id = update.effective_chat.id
     logging.info(chat_id)
     logging.info(text)
