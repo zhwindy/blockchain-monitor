43d5a874d52224686cd60d6e03bb3323907159e1
zhwindy <kmust091314@gmail.com>
2022-04-13 16:59:47 +0800

    fix ip

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def bsc2(update: Update, context: CallbackContext):
     node_data = get_newest_block(url)
     block_height = get_block_height(node_data)
     block_time = get_block_time(node_data)
-    text = f"节点: bnb-node-02\nip地址: 10.*.*.172\n最新高度: {block_height}\n出块时间: {block_time}"""
+    text = f"节点: bnb-node-02\nip地址: 10.*.*.99\n最新高度: {block_height}\n出块时间: {block_time}"""
     chat_id = update.effective_chat.id
     logging.info(text)
     context.bot.send_message(chat_id=chat_id, text=text)
