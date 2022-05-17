a8e79b62ff19d83af4c62a0b649d10ec54a48911
zhwindy <kmust091314@gmail.com>
2022-05-17 20:46:30 +0800

    fix import issue

--- crontab/monitor_polygon_node.py
+++ crontab/monitor_polygon_node.py
@@ -1,5 +1,8 @@
 # encoding=utf-8
-from common import get_newest_block, get_block_time, get_block_height, send_news
+import sys
+sys.path.append("..")
+from utils import rpc
+
 
 node_list = {
     "polygon-node-01": "172.31.28.220",
@@ -11,13 +14,14 @@ def main():
     for key, ip in node_list.items():
         try:
             url = f"http://{ip}:8545"
-            node_data = get_newest_block(url)
-            block_height = get_block_height(node_data)
-            block_time = get_block_time(node_data)
+            node_data = rpc.get_newest_block(url)
+            block_height = rpc.get_block_height(node_data)
+            block_time = rpc.get_block_time(node_data)
             content = f"{key}:{ip} \n最新高度: {block_height}\n出块时间: {block_time}"""
         except Exception as e:
             content = str(e)
-        send_news(content)
+        print(content)
+        rpc.send_news(content)
 
 
 if __name__ == '__main__':
