5b8d0b28f369aeb1f9fa241933093a3d35e4dda9
zhwindy <kmust091314@gmail.com>
2022-05-17 21:06:22 +0800

    improve bnb

--- crontab/monitor_bsc_node.py
+++ crontab/monitor_bsc_node.py
@@ -1,71 +1,31 @@
 # encoding=utf-8
-import requests
-import pytz
-from datetime import datetime
+import sys
+sys.path.append("..")
+from utils import rpc
+
 
 node_list = {
-    "bnb-node-01": "http://10.0.0.139:8545",
-    "bnb-node-02": "http://10.0.0.99:8545",
+    "bnb-node-01": {"ip": "10.0.0.139", "port": 8545},
+    "bnb-node-02": {"ip": "10.0.0.99", "port": 8545},
 }
-
-def send_news(content):
-    """
-    发送通知
-    """
-    message = {
-        "at": {
-            "isAtAll": False
-        },
-        "text": {
-            "content": content
-        },
-        "msgtype": "text"
-    }
-    url = "https://oapi.dingtalk.com/robot/send?access_token=601e6864aa1dcd0e07e1fb61227b114a32ebfc9c2c335689b9a6b473397b0bd3"
-    requests.post(url=url, json=message)
-
-
-def get_newest_block(url):
-    param = {
-        "jsonrpc": "2.0",
-        "method": "eth_getBlockByNumber",
-        "params": ["latest", False],
-        "id": 1
-    }
-    try:
-        res = requests.post(url, json=param, timeout=30)
-        result = res.json()
-        data = result.get("result", {})
-    except Exception as e:
-        data = {}
-    return data
-
-
-def get_block_height(data):
-    block_height = data.get("number", 1)
-    height = int(block_height, base=16)
-    return height
-
-
-def get_block_time(data):
-    time = data.get("timestamp", "1000000000")
-    timestamp = int(time, base=16)
-    date_time = datetime.fromtimestamp(timestamp, pytz.timezone('Asia/Shanghai'))
-    time = date_time.strftime("%Y-%m-%d %H:%M:%S")
-    return time
 
 
 def main():
-    for key, url in node_list.items():
+    for node, config in node_list.items():
         try:
-            node_data = get_newest_block(url)
-            block_height = get_block_height(node_data)
-            block_time = get_block_time(node_data)
-            content = f"monitor节点: {key}\n最新高度: {block_height}\n出块时间: {block_time}"""
+            ip = config.get("ip")
+            port = config.get("port")
+            if (not ip) or (not port):
+                continue
+            url = f"http://{ip}:{port}"
+            node_data = rpc.get_newest_block(url)
+            block_height = rpc.get_block_height(node_data)
+            block_time = rpc.get_block_time(node_data)
+            content = f"{node}: {ip} \n最新高度: {block_height}\n出块时间: {block_time}"""
         except Exception as e:
             content = str(e)
-        # print(content)
-        send_news(content)
+        print(content)
+        rpc.send_news(content)
 
 
 if __name__ == '__main__':
