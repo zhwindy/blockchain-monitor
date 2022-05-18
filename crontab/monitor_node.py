45dfa9ea5158252dd5749b05fac62b197a989a58
zhwindy <kmust091314@gmail.com>
2022-05-18 14:37:14 +0800

    add some node

--- crontab/monitor_node.py
+++ crontab/monitor_node.py
@@ -3,19 +3,24 @@ from utils import rpc
 
 
 node_list = {
-    "polygon-node-01": "172.31.28.220",
-    "polygon-node-02": "172.31.30.198",
+    "moonbeam-node": {"ip": "172.31.23.220", "port": 30335},
+    "Arbitrum-node": {"ip": "172.31.27.235", "port": 8547},
+    "Optimism-node": {"ip": "172.31.18.109", "port": 9991},
 }
 
 
 def main():
-    for key, ip in node_list.items():
+    for node, config in node_list.items():
         try:
-            url = f"http://{ip}:8545"
+            ip = config.get("ip")
+            port = config.get("port")
+            if (not ip) or (not port):
+                continue
+            url = f"http://{ip}:{port}"
             node_data = rpc.get_newest_block(url)
             block_height = rpc.get_block_height(node_data)
             block_time = rpc.get_block_time(node_data)
-            content = f"{key}: {ip} \n最新高度: {block_height}\n出块时间: {block_time}"""
+            content = f"{node}: {ip} \n最新高度: {block_height}\n出块时间: {block_time}"""
         except Exception as e:
             content = str(e)
         print(content)
