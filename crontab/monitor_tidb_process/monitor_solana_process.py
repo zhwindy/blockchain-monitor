7cd05fac180d6e3c28e7033892b1990c9acb4382
zhwindy <kmust091314@gmail.com>
2022-12-19 21:40:02 +0800

    fix solana monitor

--- crontab/monitor_tidb_process/monitor_solana_process.py
+++ crontab/monitor_tidb_process/monitor_solana_process.py
@@ -17,7 +17,7 @@ def get_sol_data():
     database = "sol_data"
     conn = get_conn(database=database)
     cursor = conn.cursor()
-    sql = "select block_number, timestamp from avax_source_block order by block_number desc limit 1"
+    sql = "select block_number, timestamp from sol_source_block order by block_number desc limit 1"
     info = {
         "block_number": 0,
         "create_time": 0
