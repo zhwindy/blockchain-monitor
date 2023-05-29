14caa742b49ce04cf941de12885f0bf29d596e50
zhwindy <kmust091314@gmail.com>
2023-05-29 15:14:47 +0800

    fix btc monitor

--- crontab/monitor_tidb_process/monitor_btc_process.py
+++ crontab/monitor_tidb_process/monitor_btc_process.py
@@ -1,5 +1,6 @@
 # encoding=utf-8
 import datetime
+import requests
 import time
 import os
 from db_mysql import get_conn
@@ -7,6 +8,8 @@ from alert_telegram import alert
 from alert_email import send_email_alert
 
 MODE = os.getenv('NFT_MONITOR_MODE', 'dev')
+BTC_U = os.getenv('BTC_U')
+BTC_P = os.getenv('BTC_P')
 # 报警阈值
 THRESHOLD = 30
 
@@ -32,6 +35,25 @@ def get_data():
     conn.close()
     return info
 
+def get_block_height():
+    """
+    查询当前最新高度
+    """
+    url = f"http://{BTC_U}:{BTC_P}@172.31.22.119:8080"
+    params = {
+        "jsonrpc": "2.0",
+        "id":"1",
+        "method": "getblockchaininfo",
+        "params": []
+    }
+    rep = requests.post(url=url, json=params, timeout=20)
+    if rep.status_code == 200:
+        rt_data = rep.json()
+        rt = rt_data.get("result", {})
+        height = rt.get("headers", 1)
+    else:
+        height = 1
+    return height
 
 def monitor():
     info = get_data()
@@ -41,12 +63,13 @@ def monitor():
     record_timestamp = int(record_time)
     diff_seconds = max(0, now_timestamp-record_timestamp)
     diff_min = diff_seconds // 60
-    text =f"【Tidb 解析延迟告警】主链: BTC\n已解析高度: {block_number}\n当前延迟约: {diff_min}分钟"
-    now = str(datetime.datetime.now())
+    new_height = get_block_height()
+    diff_block = new_height - block_number
+    text =f"【Tidb 解析延迟告警】主链: BTC\n已解析高度: {block_number}\n当前最新高度: {new_height}\n当前延迟约: {diff_min}分钟"
     print(text)
     if MODE == 'dev':
         return None
-    if int(diff_min) > THRESHOLD:
+    if diff_block > 0:
         alert(text)
         send_email_alert("BTC", block_number, diff_min)
 
