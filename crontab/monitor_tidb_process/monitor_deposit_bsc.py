f1122507028dd2bd898daf216937195a6a4985cc
zhwindy <kmust091314@gmail.com>
2023-07-19 18:06:34 +0800

    fix some message

--- crontab/monitor_tidb_process/monitor_deposit_bsc.py
+++ crontab/monitor_tidb_process/monitor_deposit_bsc.py
@@ -48,18 +48,18 @@ def get_newest_block(url, block_number=None):
 def monitor():
     process_number = get_depoist_block_data()
     if not process_number:
-        text = f"【用户充值监测延迟告警】主链:BSC\n已解析高度获取失败, 请及时检查!"
+        text = f"【用户充值监测延迟告警】主链:BSC\n从redis查询已解析高度失败, 请及时检查!"
         diff_min = 10000
     else:
         process_number_hex = hex(process_number)
         node_block_data = get_newest_block(BSC_NODE_URL, block_number=process_number_hex)
         if not node_block_data:
-            text = f"【用户充值监测延迟告警】主链:BSC\n已解析区块的信息获取失败, 请及时检查!"
+            text = f"【用户充值监测延迟告警】主链:BSC\n通过节点查询已解析区块的信息失败, 请及时检查!"
             diff_min = 10000
         else:
             block_timestamp_hex = node_block_data.get("timestamp")
             if not block_timestamp_hex:
-                text = f"【用户充值监测延迟告警】主链:BSC\n已解析区块的出块时间失败, 请及时检查!"
+                text = f"【用户充值监测延迟告警】主链:BSC\n获取已解析区块的出块时间失败, 请及时检查!"
                 diff_min = 10000
             else:
                 now_timestamp = int(time.time())
